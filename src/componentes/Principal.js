04383f0121f84c77c8f6c08f299276a05b9874e7
KeuryLendof <lendofkeury@gmail.com>
2021-12-19 15:50:50 -0400

    imagen principal

--- src/componentes/Principal.js
+++ src/componentes/Principal.js
@@ -6,46 +6,53 @@ const Principal = () =>{
 
 
     return(
-        <div className="contenedorPrincipal">
-            <main>
-                <div className="card">
-                    <img src=""/>
-                    <h4>Cuadrado</h4>
-                    <a href="">Calcular Area</a>
+        <div>
+            <article class="hero-image">
+                <div>
+                    <h1>Calcular el area de las figuras geometricas</h1>
                 </div>
+            </article>
+            <div className="contenedorPrincipal">
+                <main>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Cuadrado</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                <div className="card">
-                    <img src=""/>
-                    <h4>Triangulo</h4>
-                    <a href="">Calcular Area</a>
-                </div>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Triangulo</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                <div className="card">
-                    <img src=""/>
-                    <h4>Circulo</h4>
-                    <a href="">Calcular Area</a>
-                </div>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Circulo</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                <div className="card">
-                    <img src=""/>
-                    <h4>Login</h4>
-                    <a href="">Calcular Area</a>
-                </div>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Login</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                <div className="card">
-                    <img src=""/>
-                    <h4>Login</h4>
-                    <a href="">Calcular Area</a>
-                </div>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Login</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                <div className="card">
-                    <img src=""/>
-                    <h4>Login</h4>
-                    <a href="">Calcular Area</a>
-                </div>
+                    <div className="card">
+                        <img src=""/>
+                        <h4>Login</h4>
+                        <a href="">Calcular Area</a>
+                    </div>
 
-                
-            </main>
+                    
+                </main>
+            </div>
         </div>
     )
 }
