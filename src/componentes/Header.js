14b16d5a9aa8cb0ac6bf2cb44559f3e9fbac987b
KeuryLendof <lendofkeury@gmail.com>
2021-12-19 17:23:12 -0400

    header 50% y mas

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,12 +1,30 @@
 import "../App.css"
 
 
-const Header =()=>{
+function Header(){
 
     return(
-        <div>
-            <header className="titulares">
-                <h2>GeoFig</h2>
+        <div className="contenedorHeader">
+            <header>
+                <div className="logo">
+                    <div className="circulo">
+
+                    </div>
+                    <p>GeoFig</p>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a>Inicio</a></li>
+                        <li><a>Nose</a></li>
+                        <li><a>Todavia nose</a></li>
+                        <li><a>Contacto</a></li>
+                    </ul>
+                </nav>
+                <button className="boton">
+                    <div className="linea"></div>
+                    <div className="linea"></div>
+                    <div className="linea"></div>
+                </button>
             </header>
         </div>
     )
