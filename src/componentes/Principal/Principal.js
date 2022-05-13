9aac71eb29fe9f78ad740d4191ba34b50b5a28ce
KeuryLendof <lendofkeury@gmail.com>
2022-05-13 17:25:30 -0400

    card 70%

--- src/componentes/Principal/Principal.js
+++ src/componentes/Principal/Principal.js
@@ -6,65 +6,63 @@ const Principal = () =>{
     //https://www.flaticon.es/packs/shapes-6?word=geometrica
 
     return(
-        <div>
-            <div className="contenedorPrincipal">
-                <main>
-                    <div className="card">
-                        <img src={require('../../assets/images/cuadrado.png')}/>
-                        <h4>Cuadrado</h4>
-                        <a href="/cuadrado">Calcular Area</a>
-                    </div>
+        <div className="contenedorPrincipal">
+            <main>
+                <div className="card">
+                    <img src={require('../../assets/images/cuadrado.png')}/>
+                    <h3>Cuadrado</h3>
+                    <a href="/cuadrado">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/triangulo.png')}/>
-                        <h4>Triangulo</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/triangulo.png')}/>
+                    <h3>Triangulo</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/circulo.png')}/>
-                        <h4>Circulo</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/circulo.png')}/>
+                    <h3>Circulo</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/rectangulo.png')}/>
-                        <h4>Rectangulo</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/rectangulo.png')}/>
+                    <h3>Rectangulo</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/rombo.png')}/>
-                        <h4>Rombo</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/rombo.png')}/>
+                    <h3>Rombo</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/trapecio.png')}/>
-                        <h4>Trapecio</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/trapecio.png')}/>
+                    <h4>Trapecio</h4>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/elipse.png')}/>
-                        <h4>Elipse</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/elipse.png')}/>
+                    <h3>Elipse</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/pentagono.png')}/>
-                        <h4>Pentagono</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
+                <div className="card">
+                    <img src={require('../../assets/images/pentagono.png')}/>
+                    <h3>Pentagono</h3>
+                    <a href="">Calcular Area</a>
+                </div>
 
-                    <div className="card">
-                        <img src={require('../../assets/images/hexagono.png')}/>
-                        <h4>Hexagono</h4>
-                        <a href="">Calcular Area</a>
-                    </div>
-                    
-                </main>
-            </div>
+                <div className="card">
+                    <img src={require('../../assets/images/hexagono.png')}/>
+                    <h3>Hexagono</h3>
+                    <a href="">Calcular Area</a>
+                </div>
+                
+            </main>
         </div>
     )
 }
