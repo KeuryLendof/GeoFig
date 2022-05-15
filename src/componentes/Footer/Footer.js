db838947203e8c29158a6cc396eb5e9fb8dd3b68
KeuryLendof <lendofkeury@gmail.com>
2022-05-14 21:24:04 -0400

    footer 20%

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -7,7 +7,17 @@ const Footer = ()=>{
 
     return(
         <footer className="footer">
-            <h2>Sticky Footer</h2>
+
+            <section>
+
+                <div className="footer__social">
+                    <a href="https://github.com/KeuryLendof" target='_blank' rel="noreferrer" className="footer__media"><img src={require('../../assets/images/github (3).png')} className="footer__img" alt=""/></a>
+                    <a href="https://twitter.com/KeuryLendof1" target='_blank' rel="noreferrer" className="footer__media"><img src={require('../../assets/images/gorjeo (2).png')} className="footer__img" alt=""/></a>
+                    <a href="https://www.linkedin.com/in/keury-lendof-599b82185/" target='_blank' rel="noreferrer" className="footer__media footer__media--last"><img src={require('../../assets/images/linkedin (1).png')} className="footer__img" alt=""/></a>
+                </div>
+
+            </section>
+
         </footer>
     )
 }
